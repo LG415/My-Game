7d51b7d18a3d360a70406511d6e120df4fc870b2
Mission Bit <missionbit@160p-MissionBit.local>
2019-07-11 14:05:13 -0700

    a lot of changes

---
+++ Demo Day Project/Assets/CameraFollow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform playerTransform;
+
+    public float offset;
+
+    void Start() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+     void LateUpdate () {
+
+        // we store current camera's position in variable temp - temporary position
+        Vector3 temp = transform.position;
+
+        // we set the camera's position x to b equal to the player's position x
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        // this will add the offset value to the temporary camera x position
+        temp.x += offset;
+
+        // we set back the camera's temp position to the camera's current position
+        transform.position = temp;
+
+    }
+
+
+        
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ array practice/Assets/Duplicator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicator : MonoBehaviour
+
+
+{
+    class: class }
+    System.Random rand;
+    int x;
+    int y;
+
+        void Start() {
+        rand = new System.Random();
+        x = rand.Next(-8, 8);
+        y = rand.Next(-5, 5);
+        Vector2 vec = new Vector2(x, y);
+        Instantiate("Jack", vec,
+        Quaternion.identity);
+           
+
+   
+
+
+    }
+}
